10de8a4ee9b4927782ab945b695bf52b3be3bcfd
DaliPazmino <dalilapazmino@gmail.com>
2025-10-20 19:40:01 -0500

    creacion de archivo test.js y la funcion saludar

---
+++ test.js
@@ -0,0 +1,9 @@
+saludar = function(){
+    let nombre = document.getElementById ("txtNombre");
+    let valorNombre = nombre.value;
+    let apellido = document.getElementById ("txtApellido");
+    let valorApellido = apellido.value;
+    let mensaje = generarSaludo(valorNombre, valorApellido);
+    console.log(mensaje);
+    return mensaje 
+}
