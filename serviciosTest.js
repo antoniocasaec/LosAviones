43d852a5809fdd338dfffbf8340ccb25726a3180
antoniocasaec <antoniocasa.ec@gmail.com>
2025-10-20 21:32:36 -0500

    Muestra el resultadogit status!

--- serviciosTest.js
+++ serviciosTest.js
@@ -1,4 +1,11 @@
-generarSaludo = function(p_nombre,p_apellido){
-    let resultado = "Hola "+p_nombre+" "+p_apellido;
+generarSaludo = function (p_nombre, p_apellido) {
+    let resultado = "Hola " + p_nombre + " " + p_apellido;
+    mostrarTexto("lbResultado",resultado);
     return resultado;
+}
+
+mostrarTexto=function(idComponente,mensaje){
+    let componente;
+    componente=document.getElementById(idComponente);
+    componente.innerText = mensaje;
 }
--- test.js
+++ test.js
@@ -1,9 +1,10 @@
-saludar = function(){
-    let nombre = document.getElementById ("txtNombre");
+saludar = function () {
+    let nombre = document.getElementById("txtNombre");
     let valorNombre = nombre.value;
-    let apellido = document.getElementById ("txtApellido");
+    let apellido = document.getElementById("txtApellido");
     let valorApellido = apellido.value;
     let mensaje = generarSaludo(valorNombre, valorApellido);
     console.log(mensaje);
-    return mensaje 
+    return mensaje
 }
+
